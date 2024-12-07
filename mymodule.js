f7ba8307340ee96b0331db2a4c4868c5f76e69ac
vish0598 <49305925+vish0598@users.noreply.github.com>
2024-12-07 17:17:19 +0530

    Update mymodule.js

--- mymodule.js
+++ mymodule.js
@@ -6,7 +6,7 @@ exports.addition=function(a,b){
 exports.writedatafile=function(nm,skills){
 	data=nm+",";
 	for(i=0;i<skills.length;i++){
-		data=data+skills[i]+",";
+		data=data+skills[i]+","+",";
 		
 	}
 	fs.appendFile("userdata.txt",data);
